b1eeeccb65ec20ed9ec0ce08012b60e46358893d
ugurctaygun <ugurctaygun@gmail.com>
2022-04-27 23:29:05 +0300

    cors3

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.use(
   express.static("public")
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/images", express.static("images"));
 
 app.get("https://upvote-case-backend.herokuapp.com/images/:id", (req, res) => {
