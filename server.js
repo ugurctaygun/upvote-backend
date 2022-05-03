303d7dd90ffcacf0942f23a016150c99b6ebd80d
ugurctaygun <ugurctaygun@gmail.com>
2022-05-03 19:24:41 +0300

    routes

--- server.js
+++ server.js
@@ -4,24 +4,12 @@ const app = express();
 const cors = require("cors");
 const puppeteer = require("puppeteer");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.use(
-  cors({
-    origin: "*",
-  }),
   express.json({
     extended: false,
-  }),
-  express.static("public")
+  })
 );
+app.use(cors());
 
 app.use("/images", express.static("images"));
 
